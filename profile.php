6ce42adf2b217e11772920fdb6e9af3b7952e9b5
Pram <87259818+pram47@users.noreply.github.com>
2023-12-19 01:03:34 +0700

    Add files via upload

--- profile.php
+++ profile.php
@@ -2,6 +2,19 @@
 
     session_start();
     include_once 'config/db.php';
+    if (isset($_GET['delete'])) {
+        $delete_id = $_GET['delete'];
+        $deletestmt = $conn->query("DELETE FROM users WHERE id = $delete_id");
+        $deletestmt->execute();
+
+        if ($deletestmt) {
+            echo "<script>alert('Data has been deleted successfully');</script>";
+            // $_SESSION['success'] = "Data has been deleted succesfully";
+            header("refresh:1; url=user.php");
+        }
+        
+    }
+
 ?>
 
 <!DOCTYPE html>
